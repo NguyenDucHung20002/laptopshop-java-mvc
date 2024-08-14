1ab8555bbc4ada55750eb130f8fb169ecbb126c7
= <=>
2024-08-14 13:47:39 +0700

    first commit

---
+++ src/main/java/vn/laptopshop/LaptopshopApplication.java
@@ -0,0 +1,16 @@
+package vn.hoidanit.laptopshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@SpringBootApplication
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
+
+public class LaptopshopApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LaptopshopApplication.class, args);
+	}
+
+}
